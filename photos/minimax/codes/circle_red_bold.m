4dd8e25bfa1b91ec1f9d3b110bbf8244f48e7002
Bhaskar Vundurthy <bvundurthy@outlook.com>
2020-07-25 02:30:00 +0530

    changes made

--- photos/minimax/codes/circle_red_bold.m
+++
@@ -1,9 +0,0 @@
-% This function plots circle with center at (x,y) and radius 'r' in the
-% form circle(x,y,r)
-function h = circle_red_bold(x,y,r)
-hold on
-th = 0:pi/50:2*pi;
-xunit = r * cos(th) + x;
-yunit = r * sin(th) + y;
-h = plot(xunit, yunit,'LineWidth',3.5,'color','r');
-end
--- photos/minimax/codes/norm2.m
+++
@@ -1,5 +0,0 @@
-function [dist] = norm2(P,Q)
-% norm2 is used to find the Euclidean distance between two points with
-% syntax norm2(P(:,1),P(:,2))
-dist=sqrt((P(1)-Q(1))^2+(P(2)-Q(2))^2);
-end
